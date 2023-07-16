7627f04ce81fd47d93947b0f2e37f187bc519692
sebabainotti <sebabainotti@gmail.com>
2023-07-16 10:29:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import Web3 from 'web3';
+import React from 'react';
 import ConnectWalletPage from './pages/connectWallet';
 import $ from 'jquery'
 import yo from './images/yo.png';
